b13510d477a52ca52d29cfeac649ed22a6647b15
Masahiro Minami <masa.m373@gmail.com>
2025-05-11 21:28:48 +0900

    fix sample chat app

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 # Github: https://github.com/naotaka1128/llm_app_codes/chapter02/main.py
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import GoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
-###### dotenv を利用しない場合は消してください ######
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except ImportError:
-    import warnings
-    warnings.warn("dotenv not found. Please make sure to set your environment variables manually.", ImportWarning)
-################################################
-
 
 def main():
     st.set_page_config(
@@ -28,10 +18,11 @@ def main():
             ("system", "You are a helpful assistant.")
         ]
 
-    # ChatGPTに質問を与えて回答を取り出す(パースする)処理を作成 (1.-4.の処理)
-    # 1. ChatGPTのモデルを呼び出すように設定
-    #    (デフォルトではGPT-3.5 Turboが呼ばれる)
-    llm = ChatOpenAI(temperature=0)
+    # LLMに質問を与えて回答を取り出す(パースする)処理を作成 (1.-4.の処理)
+    llm = GoogleGenerativeAI(
+        model = "models/gemini-1.5-flash-001-tuning",
+        google_api_key = st.secrets.GeminiKey.GOOGLE_API_KEY,
+    )
 
     # 2. ユーザーの質問を受け取り、ChatGPTに渡すためのテンプレートを作成
     #    テンプレートには過去のチャット履歴を含めるように設定
